cca3c16f151394eb2e00f65fe356b99b61f61666
DURNERIN NATHAN <durnerin.nathan01@gmail.com>
2025-03-31 18:17:22 +0200

    ajout mew

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,15 +1,29 @@
 import React from "react";
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function PokemonCard() {
+  const pokemon = pokemonList[1];
   return (
     <div>
       <figure>
-        <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-          alt="Bulbasaur"
-        />
+        {pokemon.imgSrc ? (
+          <img src={pokemon.imgSrc} alt={pokemon.name} />
+        ) : (
+          <p>???</p>
+        )}
+        <h2>{pokemon.name}</h2>
       </figure>
-      <figcaption>Bulbasaur</figcaption>
+      <figcaption>{pokemon.name}</figcaption>
     </div>
   );
 }
