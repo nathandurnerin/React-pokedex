4868c58ea87d16fc14ef073879b6e2a52e494d50
DURNERIN NATHAN <durnerin.nathan01@gmail.com>
2025-03-31 17:12:19 +0200

    ajout bulbasaur

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function PokemonCard() {
+  return (
+    <div>
+      <figure>
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+          alt="Bulbasaur"
+        />
+      </figure>
+      <figcaption>Bulbasaur</figcaption>
+    </div>
+  );
+}
+
+export default PokemonCard;
